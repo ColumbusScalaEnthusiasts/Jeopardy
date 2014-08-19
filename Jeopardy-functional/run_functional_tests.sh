977c99525b3999f8a21682cc8e62e1ccd8ca3337
Dan Wiebe <dnwiebe@gmail.com>
2014-08-19 07:27:42 -0400

    Another attempt at running functional tests in CI

---
+++ Jeopardy-functional/run_functional_tests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Usage: run_functional_test <dir> <seconds> <url>
+# where:
+# <dir>         : directory in which to find the Jeopardy project
+# <seconds>     : maximum number of seconds to wait for server to come up
+# <url>         : URL to use to feel for the server to be up
+
+get_server_pid () {
+	server_pid=$(head -n 1 "$1" | sed 's/.* \([0-9]*\)$/\1/')
+}
+
+wait_for_server () {
+	exit_code=1
+	attempts=0
+	while [ $exit_code -ne 0 -a $attempts -lt "$1" ]; do
+		curl "$2" > /dev/null
+		exit_code=$?
+		attempts=$(( attempts + 1 ))
+		sleep 1
+	done
+	return $exit_code
+}
+
+pushd "$1"
+activator stage
+target/universal/stage/bin/jeopardy >functional_test_log.txt &
+popd
+wait_for_server "$2" "$3"
+if [ $? -ne 0 ] ; then
+	get_server_pid "$1/functional_test_log.txt"
+	kill -9 $server_pid
+	exit -1
+fi
+sbt test
+exit_code=$?
+get_server_pid "$1/functional_test_log.txt"
+kill $server_pid
+exit $exit_code
+
